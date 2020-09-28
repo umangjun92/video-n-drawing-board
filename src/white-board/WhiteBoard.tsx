704b9bc55189d5719ffa0bdcc40184155bb2afc9
Umang Khandelwal <umng.khndlwl@gmail.com>
2020-09-28 15:38:57 +0530

    fix: whiteboard get position props from parent

--- src/white-board/WhiteBoard.tsx
+++ src/white-board/WhiteBoard.tsx
@@ -2,80 +2,104 @@ import React, { useRef, useState, useEffect } from "react";
 
 import useDimensions from "../hooks/useDimensions";
 
-export interface WhiteBoardProps {}
+export interface WhiteBoardProps {
+    isDrawing: boolean;
+    onStartDrawing: ({ nativeEvent }: React.MouseEvent<HTMLCanvasElement, MouseEvent>) => void;
+    onDrawing: ({ nativeEvent }: React.MouseEvent<HTMLCanvasElement, MouseEvent>) => void;
+    onFinishDrawing: () => void;
+    startPos: { x: number; y: number };
+    pointerPos: { x: number; y: number };
+}
 
-const WhiteBoard = ({}: WhiteBoardProps) => {
+const WhiteBoard = ({
+    isDrawing,
+    onStartDrawing,
+    onDrawing,
+    onFinishDrawing,
+    startPos,
+    pointerPos
+}: WhiteBoardProps) => {
     const canvasContainerRef = useRef<HTMLDivElement>(null);
     const canvasRef = useRef<HTMLCanvasElement>(null);
     const contextRef = useRef<CanvasRenderingContext2D | null>(null);
-    const [isDrawing, setIsDrawing] = useState(false);
 
     const { height: containerHeight, width: containerWidth } = useDimensions(canvasContainerRef);
-
-    const startDrawing = ({ nativeEvent }: React.MouseEvent<HTMLCanvasElement, MouseEvent>) => {
-        const { offsetX, offsetY } = nativeEvent;
-        contextRef.current?.beginPath();
-        contextRef.current?.moveTo(offsetX, offsetY);
-        setIsDrawing(true);
-    };
-
-    const finishDrawing = () => {
-        contextRef.current?.closePath();
-        setIsDrawing(false);
-    };
-
-    const draw = ({ nativeEvent }: React.MouseEvent<HTMLCanvasElement, MouseEvent>) => {
-        if (!isDrawing) {
-            return;
-        }
-        const { offsetX, offsetY } = nativeEvent;
-        contextRef.current?.lineTo(offsetX, offsetY);
-        contextRef.current?.stroke();
-    };
 
     useEffect(() => {
         const canvas = canvasRef.current;
         if (canvas) {
-            // canvas.width = window.innerWidth * 2;
-            // canvas.height = window.innerHeight * 2;
-            // canvas.style.width = `${window.innerWidth}px`;
-            // canvas.style.height = `${window.innerHeight}px`;
+            canvas.width = window.innerWidth * 2;
+            canvas.height = window.innerHeight * 2;
+            canvas.style.width = `${window.innerWidth}px`;
+            canvas.style.height = `${window.innerHeight}px`;
             const context = canvas.getContext("2d");
             if (context) {
                 context.scale(2, 2);
                 context.lineCap = "round";
-                context.strokeStyle = "black";
+                context.strokeStyle = "red";
                 context.lineWidth = 5;
                 contextRef.current = context;
             }
         }
-    }, []);
+    }, [canvasRef]);
 
     useEffect(() => {
         const canvas = canvasRef.current;
         console.log("containerHeight, ", containerHeight);
+        console.log("containerwidth, ", containerWidth);
         if (canvas) {
             canvas.height = containerHeight * 2;
             canvas.style.height = `${containerHeight}px`;
-        }
-    }, [containerHeight]);
-
-    useEffect(() => {
-        console.log("containerwidth, ", containerWidth);
-        const canvas = canvasRef.current;
-        if (canvas) {
             canvas.width = containerWidth * 2;
             canvas.style.width = `${containerWidth - 50}px`;
+            const context = canvas.getContext("2d");
+            if (context) {
+                context.scale(2, 2);
+                context.lineCap = "round";
+                context.strokeStyle = "red";
+                context.lineWidth = 5;
+                contextRef.current = context;
+            }
         }
-    }, [containerWidth]);
+    }, [containerHeight, containerWidth]);
+
+    useEffect(() => {
+        if (contextRef.current) {
+            if (isDrawing) {
+                contextRef.current.beginPath();
+                const { x, y } = pointerPos;
+                contextRef.current.moveTo(x, y);
+            } else {
+                contextRef.current.closePath();
+                return;
+            }
+        }
+    }, [isDrawing]);
+
+    useEffect(() => {
+        if (contextRef.current && isDrawing) {
+            const { x, y } = pointerPos;
+            contextRef.current.lineTo(x, y);
+            contextRef.current.stroke();
+        }
+    }, [pointerPos]);
 
     return (
         <div ref={canvasContainerRef} style={{ height: "50vh", width: "100%" }}>
             <canvas
-                style={{ border: "1px solid grey", margin: "15px" }}
-                onMouseDown={startDrawing}
-                onMouseUp={finishDrawing}
-                onMouseMove={draw}
+                // height={containerHeight * 2}
+                // width={containerWidth * 2}
+                style={{
+                    border: "1px solid grey",
+                    margin: "15px"
+                    // height: "400px",
+                    // width: "400px"
+                    // height: `${containerHeight}px`,
+                    // width: `${containerWidth - 50}px`
+                }}
+                onMouseDown={onStartDrawing}
+                onMouseUp={onFinishDrawing}
+                onMouseMove={onDrawing}
                 ref={canvasRef}
             />
         </div>
